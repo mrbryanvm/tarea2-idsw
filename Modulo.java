590a777d9f508a220c9d2f021f12f561c30fd943
SEVERNASTER <deniszeballos15@gmail.com>
2025-03-17 17:18:40 -0400

    se agrego verificacion de errores

--- Modulo.java
+++ Modulo.java
@@ -5,6 +5,10 @@ public class Modulo {
     }
 
     public int modulo(int de, int entre){
+        if (entre == 0) {
+            throw new ArithmeticException("No se puede dividir por cero.");
+        }
         return de % entre;
     }
+    
 }
