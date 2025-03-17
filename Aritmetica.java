6eb601e3328803114c1c7c286abc22607387c3cf
JHONATAN-99 <124533635+JHONATAN-99@users.noreply.github.com>
2025-03-17 18:42:40 -0400

    DE BINARIO A DECIMAL

--- Aritmetica.java
+++ Aritmetica.java
@@ -1,11 +1,25 @@
-public class Aritmetica{
+public class Aritmetica {
 
-   public double division(double a, double b) {
+    public double division(double a, double b) {
         if (b != 0) {
             return a / b;
         } else {
             System.out.println("Error");
             return 0;
         }
+    }
+
+    public int binarioDecimal(String bin){
+        int res = 0;
+        for(int i = 0;i< bin.length();i++){
+            int bit;
+            if (bin.charAt(i)=='1') {
+                bit = 1;
+            }else{
+                bit = 0;
+            }
+            res =  res * 2 + bit;
+        }
+        return res;
     }
 }
