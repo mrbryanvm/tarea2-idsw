fd04e75936a7edfe8066d1b0fdbce2a6956a035c
SEVERNASTER <deniszeballos15@gmail.com>
2025-03-17 19:42:27 -0400

    se agrego el codigo del modulo a Aritmetica.java

--- Aritmetica.java
+++ Aritmetica.java
@@ -1,11 +1,22 @@
-public class Aritmetica{
+public class Aritmetica {
 
-   public double division(double a, double b) {
+    public Aritmetica(){
+        
+    }
+
+    public double division(double a, double b) {
         if (b != 0) {
             return a / b;
         } else {
             System.out.println("Error");
             return 0;
         }
+    }
+
+    public int modulo(int de, int entre) {
+        if (entre == 0) {
+            throw new ArithmeticException("No se puede dividir por cero.");
+        }
+        return de % entre;
     }
 }
